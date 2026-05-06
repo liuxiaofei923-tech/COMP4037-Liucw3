16a2b58460df9267a569c1783bb5d1444a4da3d4
LIU XIAOFEI <liuxiaofei@LIUdeMacBook-Air.local>
2026-05-06 16:22:44 +0100

    Add 10 papers

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,108 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
+    "hu2024optimization": {
+        "author": "Hu, Dieli and Yuan, Mingang and Huang, Gaofei and Zhao, Sai and Tang, Dong",
+        "journal": "IEEE Internet of Things Journal",
+        "number": "5",
+        "pages": "5500--5514",
         "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "title": "Optimization of vehicular edge computing under time-varying fading channels with path prediction",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "12",
+        "year": "2024"
+    },
+    "jain2025intelligent": {
+        "abstract": "As a cornerstone of the global economy, the automotive industry requires intelligent and reliable maintenance approaches. Predictive maintenance provides a proactive mechanism for timely fault detection, minimized operational interruptions, and prolonged vehicle lifespan. Traditional cloud-based predictive maintenance solutions, however, face latency, bandwidth constraints, and limited scalability challenges in real-time applications. This paper proposes FogBayes, an intelligent predictive maintenance system built on a three-layer Edge\u2013Fog\u2013Cloud architecture to balance computation and minimize latency, employing optimized multivariate machine learning models at its core. The proposed system model achieves a prediction accuracy of 98.88% and an AUC ( Area under curve) of 0.9830, outperforming several baseline approaches. To ensure transparency and reproducibility, the complete source code, datasets, and experimental configurations used in this study are publicly available at https://github.com/TakMashhido/FogBayes.",
+        "author": "Jain, Preeti and Mondal, Koustav Kumar and Das, Debasis and Khandelwal, Arpit",
+        "doi": "https://doi.org/10.1007/s00607-025-01585-x",
+        "journal": "Computing",
+        "number": "12",
+        "pages": "225",
+        "publisher": "Springer",
+        "title": "Intelligent predictive maintenance: multivariate ML model optimization in an Edge-Fog-Cloud environment",
+        "type": "article",
+        "url": "https://link.springer.com/article/10.1007/s00607-025-01585-x",
+        "volume": "107",
+        "year": "2025"
+    },
+    "radenkovic2012efficient": {
+        "author": "Radenkovic, Milena and Grundy, Andrew",
+        "journal": "Ad Hoc Networks",
+        "number": "7",
+        "pages": "1322--1345",
+        "publisher": "Elsevier",
+        "title": "Efficient and adaptive congestion control for heterogeneous delay-tolerant networks",
+        "type": "article",
+        "volume": "10",
+        "year": "2012"
+    },
+    "radenkovic2019enabling": {
+        "author": "Radenkovic, Milena and Huynh, Vu San Ha and John, Robert and Manzoni, Pietro",
+        "booktitle": "2019 International Conference on Wireless and Mobile Computing, Networking and Communications (WiMob)",
+        "organization": "IEEE",
+        "pages": "1--6",
+        "title": "Enabling real-time communications and services in heterogeneous networks of drones and vehicles",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "radenkovic2020cognitive": {
+        "author": "Radenkovic, Milena and Huynh, Vu San Ha",
+        "journal": "IEEE access",
+        "pages": "179561--179574",
+        "publisher": "IEEE",
+        "title": "Cognitive caching at the edges for mobile social community networks: A multi-agent deep reinforcement learning approach",
+        "type": "article",
+        "volume": "8",
+        "year": "2020"
+    },
+    "radenkovic2020energy": {
+        "author": "Radenkovic, Milena and Huynh, Vu San Ha",
+        "booktitle": "2020 Fifth International Conference on Fog and Mobile Edge Computing (FMEC)",
+        "organization": "IEEE",
+        "pages": "5--12",
+        "title": "Energy-aware opportunistic charging and energy distribution for sustainable vehicular edge and fog networks",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "vasavi2021predictive": {
+        "author": "Vasavi, S and Aswarth, K and Pavan, T Sai Durga and Gokhale, A Anu",
+        "journal": "Materials Today: Proceedings",
+        "pages": "8645--8654",
+        "publisher": "Elsevier",
+        "title": "Predictive analytics as a service for vehicle health monitoring using edge computing and AK-NN algorithm",
+        "type": "article",
+        "volume": "46",
+        "year": "2021"
+    },
+    "xiao2021adaptivefog": {
+        "author": "Xiao, Yong and Krunz, Marwan",
+        "journal": "IEEE Transactions on Mobile Computing",
+        "number": "12",
+        "pages": "4187--4200",
+        "publisher": "IEEE",
+        "title": "AdaptiveFog: A modelling and optimization framework for fog computing in intelligent transportation systems",
+        "type": "article",
+        "volume": "21",
+        "year": "2021"
+    },
+    "yousefpour2019all": {
+        "author": "Yousefpour, Ashkan and Fung, Caleb and Nguyen, Tam and Kadiyala, Krishna and Jalali, Fatemeh and Niakanlahiji, Amirreza and Kong, Jian and Jue, Jason P",
+        "journal": "Journal of systems architecture",
+        "pages": "289--330",
+        "publisher": "Elsevier",
+        "title": "All one needs to know about fog computing and related edge computing paradigms: A complete survey",
+        "type": "article",
+        "volume": "98",
+        "year": "2019"
+    },
+    "zakhary2013efficient": {
+        "author": "Zakhary, Sameh and Radenkovic, Milena and Benslimane, Abderrahim",
+        "journal": "IEEE transactions on vehicular technology",
+        "number": "2",
+        "pages": "893--906",
+        "publisher": "IEEE",
+        "title": "Efficient location privacy-aware forwarding in opportunistic mobile networks",
+        "type": "article",
+        "volume": "63",
+        "year": "2013"
     }
 };
